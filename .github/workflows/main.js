945370cdc80cc9382e44a34300a6a2e85066c573
junheegom-hub <junheegom@gmail.com>
2026-05-02 19:13:00 +0900

    Create main.js file with Electron window setup

--- .github/workflows/main.js
+++ .github/workflows/main.js
@@ -1,12 +1,11 @@
-const { app, BrowserWindow } = require("electron");
-
-function createWindow() {
-  const win = new BrowserWindow({
-    width: 400,
-    height: 300
-  });
-
-  win.loadFile("index.html");
-}
-
-app.whenReady().then(createWindow);
+- name: main.js 생성
+  shell: powershell
+  run: |
+    @"
+    const { app, BrowserWindow } = require('electron');
+    function createWindow() {
+      const win = new BrowserWindow({ width: 400, height: 300 });
+      win.loadFile('index.html');
+    }
+    app.whenReady().then(createWindow);
+    "@ | Out-File -Encoding utf8 main.js
