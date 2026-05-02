73a1ee255c97c3590ee331056ee99edc6231e39c
junheegom-hub <junheegom@gmail.com>
2026-05-02 19:39:22 +0900

    Initialize Electron app with main window setup

---
+++ main.js
@@ -0,0 +1,12 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 400,
+    height: 300
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
